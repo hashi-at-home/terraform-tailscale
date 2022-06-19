b9384262da7eb1b0bd3bf1a9e62a07f9f80106e7
Bruce Becker <brucellino@protonmail.com>
2022-06-19 15:14:40 +0200

    feat(tailscale,vault): add vault secret for tailscale keys
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

--- main.tf
+++ main.tf
@@ -40,13 +40,36 @@ data "consul_nodes" "nodes" {
   }
 }
 
+# Create device tags for each node
+# resource "tailscale_device_tags" "device_tags" {
+#   for_each      = toset(data.consul_nodes.nodes.nodes[*].name)
+#   device_id = each.key
+#   tags = ["datacenter:${var.datacenter}"]
+# }
+
 # Create a tailscale api key for each node
 resource "tailscale_tailnet_key" "node_key" {
   for_each      = toset(data.consul_nodes.nodes.nodes[*].name)
   reusable      = true
   ephemeral     = true
   preauthorized = true
+  # tags = toset(["datacenter:${var.datacenter}", "node:${each.key}"])
 
+  # depends_on = [tailscale_device_tags.device_tags]
+}
+
+
+
+# Store the keys in Vault
+resource "vault_kv_secret_v2" "tailscale" {
+  mount = "hashiatho.me-v2"
+  name  = "tailscale_access_keys"
+
+  data_json = jsonencode(
+    {
+      for k in keys(tailscale_tailnet_key.node_key) : k => tailscale_tailnet_key.node_key[k].key
+    }
+  )
 }
 
 output "nodes" {
