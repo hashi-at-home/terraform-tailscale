6ce7caabd3c3cf7ddfc1d74f0605dd2a7a6bc560
Bruce Becker <brucellino@protonmail.com>
2022-06-19 15:17:45 +0200

    feat(consul): add consul provider (#3)
    * feat(consul): add consul provider
    
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>
    
    * feat(tailscale,consul): use consul catalog to create tailscale keys
    
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>
    
    * feat(tailscale,vault): add vault secret for tailscale keys
    
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>
    
    * feat(tailscale,vault): add vault secret for tailscale keys
    
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

--- main.tf
+++ main.tf
@@ -9,6 +9,11 @@ terraform {
       source  = "hashicorp/vault"
       version = "3.7.0"
     }
+
+    consul = {
+      source  = "hashicorp/consul"
+      version = "2.15.1"
+    }
   }
 
   backend "consul" {
@@ -25,4 +30,47 @@ data "vault_generic_secret" "tailscale" {
 provider "tailscale" {
   api_key = data.vault_generic_secret.tailscale.data.tailscale_secret_key
   tailnet = data.vault_generic_secret.tailscale.data.tailnet
+}
+
+provider "consul" {}
+
+data "consul_nodes" "nodes" {
+  query_options {
+    datacenter = var.datacenter
+  }
+}
+
+# Create device tags for each node
+# resource "tailscale_device_tags" "device_tags" {
+#   for_each      = toset(data.consul_nodes.nodes.nodes[*].name)
+#   device_id = each.key
+#   tags = ["datacenter:${var.datacenter}"]
+# }
+
+# Create a tailscale api key for each node
+resource "tailscale_tailnet_key" "node_key" {
+  for_each      = toset(data.consul_nodes.nodes.nodes[*].name)
+  reusable      = true
+  ephemeral     = true
+  preauthorized = true
+  # tags = toset(["datacenter:${var.datacenter}", "node:${each.key}"])
+  # depends_on = [tailscale_device_tags.device_tags]
+}
+
+
+
+# Store the keys in Vault
+resource "vault_kv_secret_v2" "tailscale" {
+  mount = "hashiatho.me-v2"
+  name  = "tailscale_access_keys"
+
+  data_json = jsonencode(
+    {
+      for k in keys(tailscale_tailnet_key.node_key) : k => tailscale_tailnet_key.node_key[k].key
+    }
+  )
+}
+
+output "nodes" {
+  value = length(data.consul_nodes.nodes.nodes)
 }
---
+++ variables.tf
@@ -0,0 +1,7 @@
+# datacenter is a string variable with default dc1.
+# it is not sensitive
+
+variable "datacenter" {
+  type    = string
+  default = "dc1"
+}
